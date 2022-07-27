7d925255d81c32d2022faca9e4f754380bb9dfc3
Chau Tran <nartc7789@gmail.com>
2022-07-27 14:47:00 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,54 @@
-import { Component } from "@angular/core";
+import { NgtCanvas, NgtVector3 } from "@angular-three/core";
+import { NgtBoxGeometry } from "@angular-three/core/geometries";
+import { NgtMeshBasicMaterial } from "@angular-three/core/materials";
+import { NgtMesh } from "@angular-three/core/meshes";
+import { NgtSobaOrbitControls } from "@angular-three/soba/controls";
+import { Component, Input, OnInit } from "@angular/core";
+import { Mesh } from "three/src/Three";
+
+@Component({
+  selector: "app-cube",
+  standalone: true,
+  imports: [NgtMesh, NgtBoxGeometry, NgtMeshBasicMaterial],
+  template: `
+    <ngt-mesh
+      [position]="position"
+      (pointerover)="hovered = true"
+      (pointerout)="hovered = false"
+      (beforeRender)="onBeforeRender($event.object)"
+    >
+      <ngt-box-geometry></ngt-box-geometry>
+      <ngt-mesh-basic-material
+        [color]="hovered ? 'turquoise' : 'tomato'"
+      ></ngt-mesh-basic-material>
+    </ngt-mesh>
+  `,
+})
+export class CubeComponent {
+  @Input() position?: NgtVector3;
+
+  hovered = false;
+
+  onBeforeRender(object: Mesh) {
+    object.rotation.x += 0.01;
+    object.rotation.y += 0.01;
+  }
+}
 
 @Component({
   selector: "app-root",
   standalone: true,
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>Welcome to {{ title }}!</h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img
-        width="300"
-        alt="Angular Logo"
-        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
-      />
-    </div>
-    <h2>Here are some links to help you start:</h2>
-    <ul>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/tutorial"
-            >Tour of Heroes</a
-          >
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://angular.io/cli"
-            >CLI Documentation</a
-          >
-        </h2>
-      </li>
-      <li>
-        <h2>
-          <a target="_blank" rel="noopener" href="https://blog.angular.io/"
-            >Angular blog</a
-          >
-        </h2>
-      </li>
-    </ul>
+    <ngt-canvas>
+      <app-cube [position]="[1.5, 0, 0]"></app-cube>
+      <app-cube [position]="[-1.5, 0, 0]"></app-cube>
+
+      <ngt-soba-orbit-controls></ngt-soba-orbit-controls>
+    </ngt-canvas>
   `,
   styles: [],
+  imports: [NgtCanvas, CubeComponent, NgtSobaOrbitControls],
 })
-export class AppComponent {
-  title = "ngthree";
+export class AppComponent implements OnInit {
+  ngOnInit() {}
 }
